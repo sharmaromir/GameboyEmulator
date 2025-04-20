e7986dbc849e3c0e2578a264caefd64d72115bac
Ashley Feng <ashley.c.feng@gmail.com>
2025-04-20 13:54:44 -0500

    Revert "commented out double main"
    This reverts commit 7de9e0f1626ed1576356edc5c046b455f172872d.

--- screen.cc
+++ screen.cc
@@ -4,47 +4,38 @@
 static const int windowWidth = 160;
 static const int windowHeight = 144;
 
-void render_game() {
+int main(int argc, char** argv) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    if (SDL_SetVideoMode(windowWidth, windowHeight, 32, SDL_OPENGL) == NULL) {
+        fprintf(stderr, "Failed to set video mode: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    glViewport(0, 0, windowWidth, windowHeight);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glOrtho(0, windowWidth, windowHeight, 0, -1.0, 1.0);
+
+    glClearColor(0, 0, 0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
- 	glLoadIdentity();
- 	glRasterPos2i(-1, 1);
-	glPixelZoom(1, -1);
- 	glDrawPixels(160, 144, GL_RGB, GL_UNSIGNED_BYTE, NULL); // SCREEN DATA GOES HERE
-	SDL_GL_SwapBuffers( ) ;
+    glShadeModel(GL_FLAT);
+
+    glEnable(GL_TEXTURE_2D);
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DITHER);
+    glDisable(GL_BLEND);
+
+    SDL_GL_SwapBuffers();
+
+    SDL_Delay(3000);
+
+    SDL_Quit();
+    return 0;
 }
-
-// int main(int argc, char** argv) {
-//     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-//         fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
-//         return 1;
-//     }
-
-//     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-//     if (SDL_SetVideoMode(windowWidth, windowHeight, 32, SDL_OPENGL) == NULL) {
-//         fprintf(stderr, "Failed to set video mode: %s\n", SDL_GetError());
-//         return 1;
-//     }
-
-//     glViewport(0, 0, windowWidth, windowHeight);
-//     glMatrixMode(GL_MODELVIEW);
-//     glLoadIdentity();
-//     glOrtho(0, windowWidth, windowHeight, 0, -1.0, 1.0);
-
-//     glClearColor(0, 0, 0, 1.0);
-//     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//     glShadeModel(GL_FLAT);
-
-//     glEnable(GL_TEXTURE_2D);
-//     glDisable(GL_DEPTH_TEST);
-//     glDisable(GL_CULL_FACE);
-//     glDisable(GL_DITHER);
-//     glDisable(GL_BLEND);
-
-//     SDL_GL_SwapBuffers();
-
-//     SDL_Delay(3000);
-
-//     SDL_Quit();
-//     return 0;
-// }
